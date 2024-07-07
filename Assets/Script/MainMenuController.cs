478dcf98372abec98ca6750c3d0258cbcb431cad
AMIZY17 <adamoudich17@gmail.com>
2024-07-07 14:12:22 +0200

    j'ai changé dans le script le nom de la scene et j'ai mit la bonne

--- Assets/Script/MainMenuController.cs
+++ Assets/Script/MainMenuController.cs
@@ -8,7 +8,7 @@ public class MainMenuController : MonoBehaviour
     public void LoadGame(int input)
     {
         StateNameController.seeds = input;
-        SceneManager.LoadScene("Level Test");
+        SceneManager.LoadScene("MainEndlessScene");
     }
     // Start is called before the first frame update
     void Start()
